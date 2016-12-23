c7b8bf3cfbca4120bcd29c9cc91d6833d7597c2e
Angie Yang <angieyang@Angies-MacBook-Pro.local>
2016-12-23 12:46:14 -0500

    fix bug in book-list

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,6 @@
 function selectBook(book) {
-	console.log('A book has been selected', book.title);
-	
+	return {
+		type: 'BOOK_SELECTED',
+		payload: book
+	};
 }
--- src/containers/book-list.js
+++ src/containers/book-list.js
@@ -1,13 +1,18 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
-import {selectBookk} from '../actions/index';
+import {selectBook} from '../actions/index';
 import { bindActionCreators} from 'redux';
 
 class BookList extends Component {
 	renderList() {
 		return this.props.books.map((book) => {
 			return (
-				<li key={book.title} className="list-group-item">{book.title}</li>
+				<li 
+					key={book.title} 
+					onCLick={() => this.props.selectBook(book)}
+					className="list-group-item">
+					{book.title}
+				</li>
 			)
 		})
 	}
@@ -22,10 +27,17 @@ class BookList extends Component {
 }
 
 function mapStateToProps(state) {
+	//what ever is returned will show up as props in side of BookList
 	return {
-		books: state.books;
+		books: state.books
 	};
 
 }
 
-export default connect(mapStateToProps)(BookList);
+//Anything returned from this fxn willl end up as props on the BookList container
+function mapDispatchToProps(dispatch){
+	//Whenever selectBook is called, the result should be passed to all of our reducers
+	return bindActionCreators({selectBook: selectBook}, dispatch)
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(BookList);
